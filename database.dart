00e517a5c9edba2cc49c6a2047041bec5645ef1d
aastha612-yadav <aasthayadav815@gmail.com>
2024-06-29 14:11:50 +0530

    Add files via upload

---
+++ database.dart
@@ -0,0 +1,27 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+
+  List toDoList = [];
+
+  // reference our box
+  final _myBox = Hive.box('mybox');
+
+  // opening app for the first time
+  void createinitialData(){
+    toDoList = [
+      ["Exercise", false],
+      ["Work",false],
+    ];
+  }
+
+  // load the data from database
+  void loadData(){
+    toDoList=_myBox.get("TODOLIST");
+  }
+
+// update the database
+  void updateDatabase(){
+    _myBox.put("TODOLIST",toDoList);
+  }
+}
